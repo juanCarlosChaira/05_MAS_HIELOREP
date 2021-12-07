94b295d883673f1f9294018abae13f7a2235c742
juanCarlosChaira <94226715+juanCarlosChaira@users.noreply.github.com>
2021-12-07 20:31:32 +0100

    Update HolaMundo.java
    la lla lla

--- Desktop/lito/DAW/PRAC_JAVA/HolaMundo/src/holamundo/HolaMundo.java
+++ Desktop/lito/DAW/PRAC_JAVA/HolaMundo/src/holamundo/HolaMundo.java
@@ -19,6 +19,8 @@ public class HolaMundo {
     System.out.println("holassssshhhhh");
     
     System.out.println("que tal");
+         System.out.println("que tal");
+         System.out.println("que tal");
     
     }
     
