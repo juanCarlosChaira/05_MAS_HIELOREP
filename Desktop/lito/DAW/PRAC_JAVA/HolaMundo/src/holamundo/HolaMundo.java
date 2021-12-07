2c668cd362213d36730992fc3200d6bc70a2272e
juanCarlosChaira <94226715+juanCarlosChaira@users.noreply.github.com>
2021-12-07 20:22:03 +0100

    Update HolaMundo.java
    nube commit

--- Desktop/lito/DAW/PRAC_JAVA/HolaMundo/src/holamundo/HolaMundo.java
+++ Desktop/lito/DAW/PRAC_JAVA/HolaMundo/src/holamundo/HolaMundo.java
@@ -16,7 +16,7 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    System.out.println("holasss");
+    System.out.println("holassssshhhhh");
     
     
     
